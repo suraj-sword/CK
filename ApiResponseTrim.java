69da7fd2126c698fc895fb2a7e2af81e88152bff
suraj-sword <suraj.mishra.2403@gmail.com>
2025-03-13 09:24:01 +0530

    Update ApiResponseTrim.java

--- ApiResponseTrim.java
+++ ApiResponseTrim.java
@@ -60,7 +60,7 @@ public class ApiResponseTrim {
             .header("auth-module", "1")
             .header("auth-mav", "2821")
             .header("Auth-Partner", "2000009")
-            .header("Authorization", "Bearer RBkL047SOKK17tPaUNT5kkn1pQwK0AWtG/3iK0wCujunGTTLtboDszauXmLHOrnE")
+            .header("Authorization", "Bearer RGTTLtboDszauXmLHOrnE")
             .body("{\"startDate\":1722450600000,\"endDate\":1730399399999,\"groupBy\":\"SERVICE\",\"period\":\"MONTHLY\",\"filterDetailMap\":{}}")
             .when()
             .post(apiEndpoint);
